5cbf92b8ce415f5540ab7d9e942781feef774ed7
pavmolo <74321783+pavmolo@users.noreply.github.com>
2024-05-03 20:44:06 +0300

    Update audit.py

--- audit.py
+++ audit.py
@@ -8,31 +8,30 @@ url = "https://docs.google.com/spreadsheets/d/17Fq2KaMbp_KiQ-RZcEHIMU9bjiFYYYF4j
 data = pd.read_csv(url)
 
 # Отображаем данные в Streamlit
-st.title('Данные аудита продажников')
-st.dataframe(data)
 
 # Инициализация total_vector с нулями
 total_vector = [0] * 5  # Предполагаем, что длина каждого option_vector равна 5
 
-for index, row in data.iterrows():
-    with st.container():
-        st.subheader(f'{row["Вопрос"]}:')
-        default = "Затрудняюсь ответить"
-        choice = st.radio(f'{row["Вопрос"]}:', options=[row['Левый'], default, row['Правый']], index=1, label_visibility='hidden')
-        numbers = [row['Волк'], row['Работяга'], row['Строитель Отношений'], row['Чемпион'], row['Решатель Проблем']]
-        numbers = [2*x for x in numbers]
-        inverted_numbers = [-x for x in numbers]
-        transformed_numbers = [1 if x == 0 else -1 for x in numbers]
-        
-        if choice == row['Левый']:
-            option_vector = numbers
-        elif choice == row['Правый']:
-            option_vector = inverted_numbers
-        elif choice == default:
-            option_vector = transformed_numbers
-        
-        # Суммирование option_vector с total_vector поэлементно
-        total_vector = [sum(x) for x in zip(total_vector, option_vector)]
+with st.sidebar:
+    for index, row in data.iterrows():
+        with st.container():
+            st.subheader(f'{row["Вопрос"]}:')
+            default = "Затрудняюсь ответить"
+            choice = st.radio(f'{row["Вопрос"]}:', options=[row['Левый'], default, row['Правый']], index=1, label_visibility='hidden')
+            numbers = [row['Волк'], row['Работяга'], row['Строитель Отношений'], row['Чемпион'], row['Решатель Проблем']]
+            numbers = [2*x for x in numbers]
+            inverted_numbers = [-x for x in numbers]
+            transformed_numbers = [1 if x == 0 else -1 for x in numbers]
+            
+            if choice == row['Левый']:
+                option_vector = numbers
+            elif choice == row['Правый']:
+                option_vector = inverted_numbers
+            elif choice == default:
+                option_vector = transformed_numbers
+            
+            # Суммирование option_vector с total_vector поэлементно
+            total_vector = [sum(x) for x in zip(total_vector, option_vector)]
 
 # Отображаем итоговый total_vector в Streamlit
 keys = ['Волк', 'Работяга', 'Строитель Отношений', 'Чемпион', 'Решатель Проблем']
