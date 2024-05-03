d394914b4e744aa6f0e96313759421d5f1a56c52
pavmolo <74321783+pavmolo@users.noreply.github.com>
2024-05-03 15:21:45 +0300

    Create audit.py

---
+++ audit.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import pandas as pd
+
+# Функция для загрузки данных из Excel
+def load_data():
+    url = "https://github.com/pavmolo/salesforce_audit/blob/main/autit_table.xlsx?raw=true"
+    df = pd.read_excel(url, sheet_name='Лист1')
+    return df
+
+# Загружаем данные
+data = load_data()
+
+# Отображаем данные в Streamlit
+st.title('Данные аудита продажников')
+st.dataframe(data)
